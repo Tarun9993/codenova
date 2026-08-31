cbd3296864a0f08e537c44a1ee1b7973945e8cac
Tarun <tarunkumartadala@gmail.com>
2026-08-31 22:22:09 +0530

    Allow Vercel frontend in CORS

--- src/main/java/com/tarun/codenova/common/config/SecurityConfig.java
+++ src/main/java/com/tarun/codenova/common/config/SecurityConfig.java
@@ -77,7 +77,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(
                 List.of(
                         "http://localhost:3000",
-                        "http://localhost:5173"
+                        "http://localhost:5173",
+                        "https://codenova-frontend.vercel.app"
                 )
         );
 
